40f5ac723d8a9a59529d3f6620a2160dda7884d2
Mericustar <1009699847@qq.com>
2022-05-08 22:23:58 +0800

    #202205082223 update benchmark test

--- benchmark-foo/benchmark_test.go
+++ benchmark-foo/benchmark_test.go
@@ -102,3 +102,16 @@ func Benchmark_PrayOpt(b *testing.B) {
 		PrayOpt()
 	}
 }
+
+func TestPray(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Pray()
+		})
+	}
+}
