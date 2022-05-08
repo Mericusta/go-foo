068d6681acb200a16b13d0cad95f4a036afd2dad
Mericustar <1009699847@qq.com>
2022-05-08 13:41:55 +0800

    #202205081341 update benchmark test

--- benchmark-foo/benchmark_test.go
+++ benchmark-foo/benchmark_test.go
@@ -70,3 +70,35 @@ func Benchmark_arrayFunction(b *testing.B) {
 		arrayFunction(tt.args.count)
 	}
 }
+
+// ----------------------------------------------------------------
+
+func Benchmark_Pray(b *testing.B) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{
+			"benchmark",
+		},
+	}
+	b.ResetTimer()
+	for index := 0; index != len(tests); index++ {
+		Pray()
+	}
+}
+
+func Benchmark_PrayOpt(b *testing.B) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{
+			"benchmark_opt",
+		},
+	}
+	b.ResetTimer()
+	for index := 0; index != len(tests); index++ {
+		PrayOpt()
+	}
+}
