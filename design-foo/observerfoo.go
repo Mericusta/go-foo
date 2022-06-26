6bc98b001a3bbdebdc726d9e1fc72f6a9ca3def0
Mericustar <1009699847@qq.com>
2022-06-26 22:59:15 +0800

    #202206262259 update observerfoo

--- design-foo/observerfoo.go
+++ design-foo/observerfoo.go
@@ -21,49 +21,42 @@ type topicData struct {
 	value int
 }
 
-var topicStack []topicData
+var runningTopicStack []topicData
+var pendingTopicStack []topicData
+var topicStackSlice []topicData
 
 var stackDeep int = 0
-var maxStackDeep int = 2
+var maxStackDeep int = 1
 
 func Report(topic int, value int) {
-	fmt.Printf("topic %v, value %v\n", topic, value)
+	td := topicData{topic: topic, value: value}
+	fmt.Printf("topic %v, value %v\n", td.topic, td.value)
 
-	// push
-	topicStack = append(topicStack, topicData{
-		topic: topic,
-		value: value,
-	})
-
-	if stackDeep < maxStackDeep && len(topicStack) > 0 {
-		_topic := topicStack[len(topicStack)-1] // pop
+	if stackDeep < maxStackDeep {
 		stackDeep++
-		switch _topic.topic {
+		switch td.topic {
 		case VALUE1:
-			if Value1Condition(_topic.value) {
+			if Value1Condition(td.value) {
 				Value1Callback()
 			}
 		case VALUE2:
-			if Value2Condition(_topic.value) {
+			if Value2Condition(td.value) {
 				Value2Callback()
 			}
 		case VALUE3:
-			if Value3Condition(_topic.value) {
+			if Value3Condition(td.value) {
 				Value3Callback()
 			}
 		}
 		stackDeep--
-		if len(topicStack) == 1 {
-			topicStack = topicStack[0:0]
-		} else {
-			topicStack = topicStack[0 : len(topicStack)-1]
-		}
+	} else {
+		topicStackSlice = append(topicStackSlice, td)
 	}
 }
 
 // ----------------------------------------------------------------
 
-var value1 int = 9
+var value1 int = 0
 
 func IncreaseValue1() {
 	value1++
@@ -71,7 +64,7 @@ func IncreaseValue1() {
 	Report(VALUE1, value1)
 }
 
-var value2 int = 9
+var value2 int = 0
 
 func IncreaseValue2() {
 	value2++
@@ -94,26 +87,27 @@ func Value1Condition(value1 int) bool {
 }
 
 func Value1Callback() {
-	fmt.Printf("Value 1 Callback\n")
+	fmt.Printf("Value 1 Callback Start\n")
 	IncreaseValue2()
+	fmt.Printf("Value 1 Callback Done\n")
 }
 
 func Value2Condition(value2 int) bool {
-	return value2 == 10
+	return value2 <= 10
 }
 
 func Value2Callback() {
-	fmt.Printf("Value 2 Callback\n")
-	for index := 0; index != 10; index++ {
-		IncreaseValue3()
-	}
+	fmt.Printf("Value 2 Callback Start\n")
+	IncreaseValue3()
+	fmt.Printf("Value 2 Callback Done\n")
 }
 
 func Value3Condition(value3 int) bool {
-	return value3 >= 10
+	return value3 <= 10
 }
 
 func Value3Callback() {
-	fmt.Printf("Value 3 Callback\n")
+	fmt.Printf("Value 3 Callback Start\n")
 	IncreaseValue1()
+	fmt.Printf("Value 3 Callback Done\n")
 }
