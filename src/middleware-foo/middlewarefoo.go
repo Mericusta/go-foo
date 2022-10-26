9ddab743343dadd1719c1c073a9833cdb8329094
yunpeng.li <yunpeng.li@vaneplus.com>
2022-10-26 18:36:06 +0800

    #202210261837 update middleware foo

--- src/middleware-foo/middlewarefoo.go
+++ src/middleware-foo/middlewarefoo.go
@@ -2,13 +2,7 @@ package middlewarefoo
 
 import "fmt"
 
-type interfaceA interface {
-	A() *implementA
-}
-
-type implementA struct{}
-
-func (ia *implementA) A() *implementA { return ia }
+// ---------------- 应用层 ----------------
 
 type interfaceB interface {
 	interfaceA
@@ -21,15 +15,6 @@ type implementB struct {
 
 func (ib *implementB) B() *implementB { return ib }
 
-func funcInterfaceA(ia interfaceA) {
-	ia.A()
-	fmt.Printf("funcInterfaceA\n")
-}
-
-func newInterfaceA() interfaceA {
-	return &implementA{}
-}
-
 func funcInterfaceB(ib interfaceB) {
 	ib.B()
 	fmt.Printf("funcInterfaceB\n")
@@ -39,29 +24,59 @@ func newInterfaceB(ia interfaceA) interfaceB {
 	return &implementB{implementA: ia.A()}
 }
 
+// ---------------- 框架层 ----------------
+// 框架层不能带有应用层的定义
+
+type implementA struct{}
+
+func (ia *implementA) A() *implementA { return ia }
+
+type interfaceA interface {
+	A() *implementA
+}
+
+func funcInterfaceA(ia interfaceA) {
+	ia.A()
+	fmt.Printf("funcInterfaceA\n")
+}
+
+func newInterfaceA() interfaceA {
+	return &implementA{}
+}
+
 type basement struct {
-	middlewareSlice []func(interfaceB)
+	middlewareSlice []*middleware
+}
+
+type middleware struct {
+	f func(interfaceB) // TODO: 框架层不能带有应用层的定义
+}
+
+func (m *middleware) do(ia interfaceA) {
+	ib := newInterfaceB(ia)
+	m.f(ib)
 }
 
 func (b *basement) handle(ia interfaceA) {
 	// 中间件包装
-	ib := newInterfaceB(ia)
 	for _, md := range b.middlewareSlice {
-		md(ib)
+		md.do(ia)
 	}
 
 	// 原始 handler
-	funcInterfaceA(ib)
+	funcInterfaceA(ia)
 }
 
 // 接口参数包装 middleware
 // 将 func(interfaceA) -> 转换成 func(interfaceB)
 func HandlerMiddlewareFoo(ia interfaceA) {
 	basement := &basement{}
-	basement.middlewareSlice = make([]func(interfaceB), 0)
-	basement.middlewareSlice = append(basement.middlewareSlice, func(ib interfaceB) {
-		fmt.Printf("custom handle\n")
-	}, funcInterfaceB)
+	basement.middlewareSlice = make([]*middleware, 0)
+	basement.middlewareSlice = append(basement.middlewareSlice, &middleware{
+		f: func(ib interfaceB) {
+			fmt.Printf("custom handle\n")
+		},
+	}, &middleware{f: funcInterfaceB})
 
 	basement.handle(ia)
 }
