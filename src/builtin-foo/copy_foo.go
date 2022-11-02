7d378882eb16cf4b9e6b67226963e9b07b8d3dc3
yunpeng.li <yunpeng.li@vaneplus.com>
2022-11-02 18:25:21 +0800

    #202211021826 update copy foo

--- src/builtin-foo/copy_foo.go
+++ src/builtin-foo/copy_foo.go
@@ -17,3 +17,23 @@ func CopyStringSliceFromStringsSplit(c int, s string, lenCase string) []string {
 	copy(strSlice, strings.Split(s, ","))
 	return strSlice
 }
+
+func CopyByteSliceFromStringWithThreeCases(c int, s string, lenCase string) []byte {
+	var byteSlice []byte
+	switch lenCase {
+	case "==": // equal
+		byteSlice = make([]byte, c)
+	case "<":
+		byteSlice = make([]byte, c-1)
+	case ">":
+		byteSlice = make([]byte, c+1)
+	}
+	copy(byteSlice, s)
+	return byteSlice
+}
+
+func CopyByteSliceFromString(s string) []byte {
+	byteSlice := make([]byte, len(s))
+	copy(byteSlice, s)
+	return byteSlice
+}
--- src/builtin-foo/copy_foo_test.go
+++ src/builtin-foo/copy_foo_test.go
@@ -41,3 +41,65 @@ func TestCopyStringSliceFromStringsSplit(t *testing.T) {
 		})
 	}
 }
+
+func TestCopyByteSliceFromStringWithThreeCases(t *testing.T) {
+	type args struct {
+		c       int
+		s       string
+		lenCase string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		// TODO: Add test cases.
+		{
+			"test case 1, equal",
+			args{c: 8, s: "a1,b2,c3", lenCase: "=="},
+			[]byte("a1,b2,c3"),
+		},
+		{
+			"test case 1, equal",
+			args{c: 8, s: "a1,b2,c3", lenCase: "<"},
+			[]byte("a1,b2,c"),
+		},
+		{
+			"test case 1, equal",
+			args{c: 8, s: "a1,b2,c3", lenCase: ">"},
+			append([]byte("a1,b2,c3"), 0),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CopyByteSliceFromStringWithThreeCases(tt.args.c, tt.args.s, tt.args.lenCase); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CopyByteSliceFromStringWithThreeCases() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCopyByteSliceFromString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		// TODO: Add test cases.
+		{
+			"test case 1",
+			args{s: "a1,b2,c3,d4,e5,f6,g8"},
+			[]byte("a1,b2,c3,d4,e5,f6,g8"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CopyByteSliceFromString(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CopyByteSliceFromString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
