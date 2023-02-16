4fcb3162c2a1609c5baafd70b695710f6a09bd6f
yunpeng.li <yunpeng.li@vaneplus.com>
2023-02-16 17:47:04 +0800

    #202302161747 add fmt

---
+++ src/builtin-foo/fmt.go
@@ -0,0 +1,130 @@
+package builtinfoo
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+type fmtStruct struct {
+	i int
+	s string
+}
+
+func FmtPtr() {
+	var i int = 1
+	var iPtr *int = &i
+	var str string = "1"
+	var strPtr *string = &str
+	var m map[int]int = map[int]int{1: 1} // as same as make(map[int]int)
+	var mPtr *map[int]int = &m
+	var slice []int = []int{1} // as same as make([]int, 0, 2)
+	var slicePtr *[]int = &slice
+	var s fmtStruct = fmtStruct{i: 1, s: "1"}
+	var sPtr *fmtStruct = &s
+
+	fmt.Println(i)
+	fmt.Println(reflect.TypeOf(i))
+	fmt.Printf("v %v\n", i)
+	fmt.Printf("p -\n")
+	fmt.Println(iPtr)
+	fmt.Println(reflect.TypeOf(iPtr))
+	fmt.Printf("v %v\n", iPtr)
+	fmt.Printf("p %p\n", iPtr)
+	fmt.Printf("&i == iPtr %v\n", &i == iPtr)
+	fmt.Println()
+
+	fmt.Printf("for integer type, including intN, floatN\n")
+	fmt.Printf("- %%v is its origin value\n")
+	fmt.Printf("- can not output %%p because it is not a pointer\n")
+	fmt.Printf("for integer type's pointer\n")
+	fmt.Printf("- %%v is equal to %%p\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Println()
+
+	fmt.Println(s)
+	fmt.Println(reflect.TypeOf(s))
+	fmt.Printf("v %v\n", s)
+	fmt.Printf("p -\n")
+	fmt.Println(sPtr)
+	fmt.Println(reflect.TypeOf(sPtr))
+	fmt.Printf("v %v\n", sPtr)
+	fmt.Printf("p %p\n", sPtr)
+	fmt.Printf("&s == sPtr %v\n", &s == sPtr)
+	fmt.Printf("sPtr == &s.i %v\n", uintptr(unsafe.Pointer(sPtr)) == uintptr(unsafe.Pointer(&s.i)))
+	fmt.Println()
+
+	fmt.Printf("for struct type\n")
+	fmt.Printf("- %%v is struct content value\n")
+	fmt.Printf("- can not output %%p because it is not a pointer\n")
+	fmt.Printf("for struct type's pointer\n")
+	fmt.Printf("- %%v is pointed value, same like output %%v struct\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Printf("- %%p is equal to struct first element's address\n")
+	fmt.Println()
+
+	fmt.Println(m)
+	fmt.Println(reflect.TypeOf(m))
+	fmt.Printf("v %v\n", m)
+	fmt.Printf("p %p\n", m)
+	fmt.Println(mPtr)
+	fmt.Println(reflect.TypeOf(mPtr))
+	fmt.Printf("v %v\n", mPtr)
+	fmt.Printf("p %p\n", mPtr)
+	fmt.Printf("*mPtr == &m %v\n", reflect.DeepEqual(*mPtr, m))
+	fmt.Println()
+
+	fmt.Printf("for map type\n")
+	fmt.Printf("- the underlying type is *hmap, a pointer to a struct named hmap\n")
+	fmt.Printf("- make func defined in runtime/map.go\n")
+	fmt.Printf("- %%v is map content value\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Printf("for map type's pointer\n")
+	fmt.Printf("- the underlying type is **hmap\n")
+	fmt.Printf("- %%v is *hmap\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Println()
+
+	fmt.Println(slice)
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Printf("v %v\n", slice)
+	fmt.Printf("p %p\n", slice)
+	fmt.Printf("first element p %p\n", &slice[0])
+	fmt.Println(slicePtr)
+	fmt.Println(reflect.TypeOf(slicePtr))
+	fmt.Printf("v %v\n", slicePtr)
+	fmt.Printf("p %p\n", slicePtr)
+	fmt.Printf("*slicePtr == &slice %v\n", reflect.DeepEqual(*slicePtr, slice))
+	fmt.Println()
+
+	fmt.Printf("for slice type\n")
+	fmt.Printf("- the underlying type is slice, a struct named slice\n")
+	fmt.Printf("- make func defined in runtime/slice.go\n")
+	fmt.Printf("- %%v is struct content value\n")
+	fmt.Printf("- %%p is different from struct, befause fmt.Printf output its first element address\n")
+	fmt.Printf("for slice type's pointer\n")
+	fmt.Printf("- %%v is pointed value, same like output %%v struct\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Println()
+
+	fmt.Println(str)
+	fmt.Println(reflect.TypeOf(str))
+	fmt.Printf("v %v\n", str)
+	fmt.Printf("p -\n")
+	fmt.Println(strPtr)
+	fmt.Println(reflect.TypeOf(strPtr))
+	fmt.Printf("v %v\n", strPtr)
+	fmt.Printf("p %p\n", strPtr)
+	fmt.Println()
+
+	fmt.Printf("for string type\n")
+	fmt.Printf("- the underlying type is stringStruct, a struct named stringStruct\n")
+	fmt.Printf("- struct defined in runtime/string.go\n")
+	fmt.Printf("- struct has a pointer, pointing to []byte, an array, element type is byte\n")
+	fmt.Printf("- %%v is array's content value\n")
+	fmt.Printf("- can not output %%p because it is not a pointer\n")
+	fmt.Printf("for string type's pointer\n")
+	fmt.Printf("- %%v is equal to %%p\n")
+	fmt.Printf("- %%p is pointer value\n")
+	fmt.Println()
+}
