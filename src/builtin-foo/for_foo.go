a16d1b601df7877a1beb4e732ee3085a0da6f43f
yunpeng.li <yunpeng.li@vaneplus.com>
2023-02-17 14:33:54 +0800

    #202302171434 update for foo

--- src/builtin-foo/for_foo.go
+++ src/builtin-foo/for_foo.go
@@ -21,6 +21,8 @@ func tmpValueAssignInForRange() {
 func localValueReassignInFor(catch bool) {
 	type tmpS struct{ i int }
 
+	type uintptrHolder struct{ p uintptr }
+	sUintptrHolderSlice := make([]*uintptrHolder, 0, 8)
 	sUintptrSlice := make([]uintptr, 0, 8)
 	sPtrSlice := make([]*tmpS, 0, 8)
 
@@ -28,10 +30,10 @@ func localValueReassignInFor(catch bool) {
 		// catch reference
 		for i := 0; i != 8; i++ {
 			s := &tmpS{i: i}
-			fmt.Printf("o ptr %v\n", uintptr(unsafe.Pointer(s)))
-			fmt.Printf("o ptr %v\n", uintptr(unsafe.Pointer(&s)))
+			fmt.Printf("ptr value %v\n", uintptr(unsafe.Pointer(s)))
+			fmt.Printf("ptr ptr %v\n", uintptr(unsafe.Pointer(&s)))
 			sUintptrSlice = append(sUintptrSlice, uintptr(unsafe.Pointer(s)))
-			// whatever it is, compiler can recognize if 's' is catched, then make up for logic
+			// whatever it is, compiler can recognize if 's' is caught, then make up for logic
 			if !catch {
 				sPtrSlice = append(sPtrSlice, s)
 			}
@@ -39,13 +41,21 @@ func localValueReassignInFor(catch bool) {
 	} else {
 		// lost reference
 		for i := 0; i != 8; i++ {
-			s := &tmpS{i: i}
-			fmt.Printf("o ptr %v\n", uintptr(unsafe.Pointer(s)))
-			fmt.Printf("o ptr %v\n", uintptr(unsafe.Pointer(&s)))
+			s := &tmpS{i: i}                                         // this will change old pointer pointed object
+			fmt.Printf("ptr value %v\n", uintptr(unsafe.Pointer(s))) // new object
+			fmt.Printf("ptr ptr %v\n", uintptr(unsafe.Pointer(&s)))  // old pointer
 			sUintptrSlice = append(sUintptrSlice, uintptr(unsafe.Pointer(s)))
+			sUintptrHolderSlice = append(sUintptrHolderSlice, &uintptrHolder{
+				p: uintptr(unsafe.Pointer(s)),
+			})
 		}
 	}
 
+	fmt.Printf("sUintptrHolderSlice = %v\n", sUintptrHolderSlice)
+	for i, p := range sUintptrHolderSlice {
+		s := (*tmpS)(unsafe.Pointer(p.p))
+		fmt.Printf("i %v s %v\n", i, s)
+	}
 	fmt.Printf("sUintptrSlice = %v\n", sUintptrSlice)
 	for i, p := range sUintptrSlice {
 		s := (*tmpS)(unsafe.Pointer(p))
