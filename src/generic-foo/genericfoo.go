4dc9e1067f60f1186b6440815955d0e645a77df2
yunpeng.li <yunpeng.li@vaneplus.com>
2023-02-22 17:27:48 +0800

    #202302221728 update genericgoo

--- src/generic-foo/genericfoo.go
+++ src/generic-foo/genericfoo.go
@@ -1,5 +1,7 @@
 package genericfoo
 
+import "fmt"
+
 type IBase interface {
 	Init()
 }
@@ -67,4 +69,61 @@ func GetIB() IB {
 func Get() {
 	_ = GetIA()
 	_ = GetIB()
+}
+
+// --------------------------------
+
+type structA struct{}
+
+func (s *structA) call() { fmt.Println("structA call") }
+
+type structB struct{}
+
+func (s *structB) call() { fmt.Println("structB call") }
+
+type typeConstraints interface{ structA | structB }
+type basicnterface interface{ call() }
+type generalInterface interface {
+	*structA | *structB
+	call()
+}
+
+func useGeneralInterface[T generalInterface](s T) {
+	s.call()
+}
+
+// // to make it work, we need 'generic general interface'
+// func canNotWork[T typeConstraints](s *T) {
+// 	// type constraints determines what operations are available on T
+// 	// it doesn't imply anything about *T
+// 	// https://stackoverflow.com/questions/71444847/go-with-generics-type-t-is-pointer-to-type-parameter-not-type-parameter
+// 	s.call() // type *T is pointer to type parameter, not type parameter
+// }
+
+// style 1: declare generic general interface inside type param scope
+func useGGIInsideTypeParamScope[
+	T typeConstraints, // type 1, use typeconstraints to constrain types
+	PT interface { // type 2, use a general interface to constrain types
+		*T     // type constraints
+		call() // method
+	}](s *T) {
+	PT(s).call()
+}
+
+// style 2: declare generic general interface outside type param scope
+type genericGeneralInterface[T typeConstraints] interface { // generic
+	*T            // type constraints
+	basicnterface // method
+}
+
+func useGGIOutsideTypeParamScope[T typeConstraints, PT genericGeneralInterface[T]](s *T) {
+	PT(s).call()
+}
+
+func generalInterfaceCall() {
+	useGeneralInterface(&structA{})
+	useGeneralInterface(&structB{})
+
+	useGGIOutsideTypeParamScope(&structA{})
+	useGGIOutsideTypeParamScope(&structB{})
 }
--- src/generic-foo/genericfoo_test.go
+++ src/generic-foo/genericfoo_test.go
@@ -67,3 +67,19 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func Test_generalInterfaceCall(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+		{
+			"test case 1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			generalInterfaceCall()
+		})
+	}
+}
